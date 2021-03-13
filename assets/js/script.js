9c87b6f84a49a62cc98824036b6baedc2b3b5382
Spencer Henry <spencerhenry94@gmail.com>
2021-03-13 12:37:41 -0500

    created functions to generate random password characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,29 @@
-// Assignment code here
+// Generate password buttom
 
+var passwordButton = document.getElementById('generate'); // selecting the HTML document's button element.
 
-// Get references to the #generate element
-var generateBtn = document.querySelector("#generate");
+passwordButton.onclick = generatePassword; // add a listener event when user click's the button.
 
-// Write password to the #password input
-function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
+function generatePassword() {
+    window.prompt("Hello");
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
+// Password generator functions
+
+function randomLower () {
+    return String.fromCharCode(Math.floor(Math.random() * 26 ) + 97);  // Returns a random lowercase letter.
+}
+
+function randomUpper () {
+    return String.fromCharCode(Math.floor(Math.random() * 26 ) + 65);  // Returns a random uppercase letter.
+}
+
+function randomNumber () {
+    return String.fromCharCode(Math.floor(Math.random() * 10 ) + 48);  // Returns a random number.
+}
+
+function randomSymbol () {
+    var symbols = '!@#$%^&*?/~<>';
+    return symbols [Math.floor(Math.random() * symbols.length)]; // Returns a random symbol from symbols variable.
+}
+
